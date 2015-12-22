ec56fc9034be068a1712f2e68e3a50d662561058
Ken Tran <loschorts@gmail.com>
2015-12-22 14:40:20 -0800

    abandon trying to refetch changes

--- frontend/components/test.jsx
+++ frontend/components/test.jsx
@@ -1,10 +1,26 @@
 var React = require('react');
-var SpotFocus = require('./spot_focus');
+var LineChart = require("react-chartjs").Line;
+var chartOptions = require('../util/chart_options');
+
+chartData = {};
+chartData.labels = ["a", "b", "c", "d"];
+chartData.datasets = [
+        {
+            label: "LABEL",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [0,1,2,3]
+        }
+];
 
 var Test = React.createClass({
-	render: function(){
-		return (<SpotFocus spotId={15}/>);
-	}
+  render: function() {
+    return <LineChart data={chartData} options={chartOptions} width="600" height="250"/>
+  }
 });
 
 module.exports = Test;
